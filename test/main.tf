572d90d2e984f86a11c76e3b645c322de39cb654
re-nan6 <iadhoy2000@gmail.com>
2024-10-09 22:14:37 +0900

    Webサーバのクラスタのデプロイ

--- test/main.tf
+++ test/main.tf
@@ -1,22 +1,29 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "0.0.0.0/0"
-  
-}
 resource "aws_instance" "example" {
-  ami = "ami-0fb653ca2d3203ac1"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [ aws_security_group.instance.id ]
+  ami                    = "ami-0fb653ca2d3203ac1"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
-  
+
   user_data_replace_on_change = true
 
   tags = {
@@ -24,20 +31,14 @@ resource "aws_instance" "example" {
   }
 }
 
-resource "aws_security_group" "example" {
-  name        = "terraform-example-instance"
-  description = "example"
-  vpc_id      = aws_vpc.main.id
-  tags = {
-    Name = "example"
+resource "aws_security_group" "instance" {
+
+  name = var.security_group_name
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-resource "aws_vpc_security_group_ingress_rule" "instance" { 
-  security_group_id =  aws_security_group.example.id
-  from_port = 8080
-  to_port = 8080
-  ip_protocol = "tcp"
-  cidr_ipv4 = "0.0.0.0/0"
-
 }
